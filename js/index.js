366c02739f98451d48b6caa09a131f46cf352592
yudiisss <yudisgannteng@gmail.com>
2025-08-17 18:10:04 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `Hi Nisvi I Have Something`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
