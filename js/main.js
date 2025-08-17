0e1facf362142dde41cfe83dc4e0d7a1c2752618
yudiisss <yudisgannteng@gmail.com>
2025-08-17 18:06:47 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY BIRTHDAY RIANI').split('')
+    const titles = ('thank u 4 lovin me').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
